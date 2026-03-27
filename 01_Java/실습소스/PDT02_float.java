1e831b15e8747b441fb99b5c2baa5459b0b0cb74
inky4832 <inky4832@daum.net>
2026-03-27 16:35:03 +0900

    Add files via upload

---
+++ 01_Java/실습소스/PDT02_float.java
@@ -0,0 +1,26 @@
+package exam01_기본형;
+
+public class PDT02_float {
+
+	public static void main(String[] args) {
+		
+		/*
+		    실수:
+		    float (4byte)
+		      3.13F, 3.14f 
+		      
+		    double (8byte)
+		 
+		 */
+		//float
+		System.out.println(3.14F);
+		System.out.println(3.14f);
+		
+		System.out.println("=====================================");
+		//double
+		System.out.println(3.14D);
+		System.out.println(3.14d);
+		System.out.println(3.14);
+		
+	}
+}
