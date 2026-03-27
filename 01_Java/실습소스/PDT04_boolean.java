e24fb0553cc947573ca10d3dfd7f970b3bb82673
inky4832 <inky4832@daum.net>
2026-03-27 16:47:46 +0900

    Add files via upload

---
+++ 01_Java/실습소스/PDT04_boolean.java
@@ -0,0 +1,13 @@
+package exam01_기본형;
+
+public class PDT04_boolean {
+
+	public static void main(String[] args) {
+		// boolean: 참/거짓처리,  true/false로 사용
+		
+		System.out.println(true);
+		System.out.println(false);
+
+	}
+
+}
